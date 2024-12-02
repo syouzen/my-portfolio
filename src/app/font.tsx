26add49b528c3e97e0304bc92a9e31702ee2a8b1
syouzen <seungyeon.choi1026@gmail.com>
2024-12-02 23:38:57 +0900

    🌱 headers

---
+++ src/app/font.tsx
@@ -0,0 +1,22 @@
+import { Noto_Sans_KR, Noto_Serif_KR, Cinzel } from "next/font/google";
+
+export const sans = Noto_Sans_KR({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sans",
+});
+
+export const serif = Noto_Serif_KR({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-serif",
+});
+
+export const serif_en = Cinzel({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-serif-en",
+});
