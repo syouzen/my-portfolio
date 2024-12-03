33970fcc717f51d4c9e10bd70db1b502fa6aed26
syouzen <seungyeon.choi1026@gmail.com>
2024-12-03 22:42:46 +0900

    🌱 introduction

--- src/app/_components/introduction.tsx
+++ src/app/_components/introduction.tsx
@@ -1,23 +1,49 @@
 import Image from "next/image";
 
 import profile from "../_assets/profile.png";
+import Link from "next/link";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 const Introduction = () => {
   return (
-    <div id="introduction" className="flex flex-col w-full items-center gap-y-6">
+    <div className="flex flex-col w-full items-center gap-y-6">
       <div className="flex w-full justify-between items-center gap-x-6">
-        <Image src={profile} alt="profile" width={256} height={256} className="mx-auto border-2 border-gray-200 p-4 rounded-full" />
+        <Image
+          src={profile}
+          alt="profile"
+          width={256}
+          height={256}
+          className="bg-gray-700 dark:bg-gray-300 mx-auto border-2 border-gray-200 p-4 rounded-full"
+        />
 
-        <div className="flex flex-col gap-y-2 p-4 mx-auto text-sm text-gray-300">
+        <div className="flex flex-col gap-y-2 p-4 mx-auto text-sm text-gray-700 dark:text-gray-300">
           <p>Name. 최승연</p>
-          <p>Email. seungyeon.choi1026@gmail.com</p>
-          <p>Phone. 010-9058-9205</p>
+          <p>
+            Email.{" "}
+            <Link href="mailto:seungyeon.choi1026@gmail.com" className="text-blue-500 underline transition-colors hover:text-blue-600">
+              seungyeon.choi1026@gmail.com
+            </Link>
+          </p>
+          <p>
+            Phone.{" "}
+            <Link href="tel:010-9058-9205" className="underline">
+              010-9058-9205
+            </Link>
+          </p>
           <p>Address. 서울시 송파구</p>
-          <p>github. https://github.com/syouzen</p>
+          <div className="mt-2 flex gap-x-2">
+            <Link href="https://github.com/syouzen" className="text-blue-500 underline transition-colors hover:text-blue-600">
+              <GitHubLogoIcon className="w-6 h-6 text-black dark:text-white" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/seung-yeon-choi-321595154/" className="text-blue-500 underline transition-colors hover:text-blue-600">
+              <LinkedInLogoIcon className="w-6 h-6 text-black dark:text-white" />
+            </Link>
+          </div>
         </div>
       </div>
 
-      <div className="mt-8 flex flex-col gap-y-4 text-sm font-light text-gray-300">
+      <div id="introduction" className="mt-8 flex flex-col gap-y-4 text-sm text-gray-700 dark:text-gray-300">
+        <h1 className="text-2xl font-bold mb-4 text-black dark:text-gray-200">소개</h1>
         <p>안녕하세요! 소통하는 개발자 ‘최승연’입니다.</p>
         <p>
           저는 시스템 개발부터 프론트 엔드까지 폭넓게 일해 왔습니다. 때로는 사이드 프로젝트에 참여하며 다양한 경험을 쌓았습니다. 그렇기에 어디에서 무엇을 하던
