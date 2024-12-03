06e93a89720112749671c8c5415bd0a4f9f40c0e
syouzen <seungyeon.choi1026@gmail.com>
2024-12-03 22:42:51 +0900

    🌱 header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,7 +5,7 @@ import ThemeButton from "./theme-button";
 const Header = () => {
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
-    const headerOffset = 64;
+    const headerOffset = 80;
     const elementPosition = element?.getBoundingClientRect().top || 0;
     const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
@@ -20,15 +20,15 @@ const Header = () => {
       <nav className="container max-w-4xl mx-auto px-6 py-3 flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <h1 className="text-2xl font-bold">최승연</h1>
-          <p className="text-sm text-gray-500">프론트엔드 개발자</p>
+          <p className="text-sm text-gray-500">소통하는 프론트엔드 개발자</p>
         </div>
 
         <div className="flex items-center gap-x-6">
           <button onClick={() => scrollToSection("introduction")} className="hover:text-blue-500 transition-colors">
             소개
           </button>
-          <button onClick={() => scrollToSection("skills")} className="hover:text-blue-500 transition-colors">
-            스킬
+          <button onClick={() => scrollToSection("skill")} className="hover:text-blue-500 transition-colors">
+            기술스택
           </button>
           <button onClick={() => scrollToSection("work-experience")} className="hover:text-blue-500 transition-colors">
             경력
