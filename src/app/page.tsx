ce295c707ac50a05d4b70236728f0b2e6c1080e7
syouzen <seungyeon.choi1026@gmail.com>
2024-12-03 22:57:25 +0900

    🌱 works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,13 @@
 import Introduction from "./_components/introduction";
 import Skill from "./_components/skill";
+import Work from "./_components/work";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-24 items-center">
       <Introduction />
       <Skill />
-      <div className="h-[256px]">
-        <h1 className="text-4xl font-bold">최승연</h1>
-        <p className="text-sm text-gray-500">프론트엔드 개발자</p>
-      </div>
-      <div className="h-[256px]">
-        <h1 className="text-4xl font-bold">최승연</h1>
-        <p className="text-sm text-gray-500">프론트엔드 개발자</p>
-      </div>
-      <div className="h-[256px]">
-        <h1 className="text-4xl font-bold">최승연</h1>
-        <p className="text-sm text-gray-500">프론트엔드 개발자</p>
-      </div>
-      <div className="h-[256px]">
-        <h1 className="text-4xl font-bold">최승연</h1>
-        <p className="text-sm text-gray-500">프론트엔드 개발자</p>
-      </div>
-      <div className="h-[256px]">
-        <h1 className="text-4xl font-bold">최승연</h1>
-        <p className="text-sm text-gray-500">프론트엔드 개발자</p>
-      </div>
-      <div className="h-[256px]">
-        <h1 className="text-4xl font-bold">최승연</h1>
-        <p className="text-sm text-gray-500">프론트엔드 개발자</p>
-      </div>
+      <Work />
     </div>
   );
 }
