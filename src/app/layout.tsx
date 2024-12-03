dc47b1b6ccaea1845fc7a9080ad3a6b2530ff319
syouzen <seungyeon.choi1026@gmail.com>
2024-12-03 21:54:21 +0900

    🌱 footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { sans, serif, serif_en } from "./font";
 import Header from "./_components/header";
 import ThemeProviders from "./_providers/theme";
+import Footer from "./_components/footer";
 
 const contents = {
   title: "최승연 | 포트폴리오",
@@ -43,8 +44,8 @@ export default function RootLayout({
       <body>
         <ThemeProviders>
           <Header />
-          <main className="container max-w-4xl mx-auto px-6 py-12 bg-gray-100">{children}</main>
-          <footer className="w-full h-20 bg-blue-500"></footer>
+          <main className="container max-w-4xl mx-auto px-6 py-12">{children}</main>
+          <Footer />
         </ThemeProviders>
       </body>
     </html>
